5108828d397e19a3ed0df4f8924aff6e2f26da19
mira-dot <rakhimova.m@gmail.com>
2024-03-24 11:13:37 +0100

    FE - pagination

--- stayhere-web/src/App.js
+++ stayhere-web/src/App.js
@@ -1,8 +1,20 @@
-import './App.css';
+import "./App.css";
+import Home from "./pages/Home";
+import AvailableRooms from "./pages/AvailableRooms";
+import Bookings from "./pages/Bookings";
+import NoPage from "./pages/NoPage";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div>Stay Here Web!</div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="available-rooms" element={<AvailableRooms />} />
+        <Route path="bookings" element={<Bookings />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
