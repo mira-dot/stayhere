17a3c1990c1894e388acffdec7c1f9e301734710
mira-dot <rakhimova.m@gmail.com>
2024-03-24 10:50:21 +0100

    initial front end app

---
+++ stayhere-web/src/pages/AvailableRooms.jsx
@@ -0,0 +1,5 @@
+const AvailableRooms = () => {
+  return <div>This is available rooms</div>;
+};
+
+export default AvailableRooms;
---
+++ stayhere-web/src/pages/Bookings.jsx
@@ -0,0 +1,5 @@
+const Bookings = () => {
+    return <div>This is bookings</div>
+}
+
+export default Bookings;
