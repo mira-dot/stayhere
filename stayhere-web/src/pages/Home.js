dba7da58d853aa81c19b552e0f646daa3e5c3b67
mira-dot <rakhimova.m@gmail.com>
2024-04-07 10:43:54 +0200

    add home img and text

--- stayhere-web/src/pages/Home.js
+++ stayhere-web/src/pages/Home.js
@@ -8,8 +8,12 @@ const Home = () => {
             </div>
             <div className="col-8">
                 <h2 className="text-center">Stay Here</h2>
-                <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+                <img className="float-start me-3" src="/images/main_image.jpg" width={600}/>
+                <p className="text-justify">
+                    <p className="text-center" style={{fontSize: "20px"}}><strong>Welcome to our cozy and charming hotel located near the city center!</strong></p>
+                    With only 10 rooms, our hotel offers a personal touch and intimate atmosphere for our guests. Our garden and free parking provide a peaceful and convenient stay for those looking to explore the city without the hassle of parking.
+                    Surrounded by entertaining and historical attractions, our hotel is the perfect base for those seeking to immerse themselves in the culture and history of the city. And with modern cafes and popular restaurants just a stone's throw away, guests can indulge in delicious dining options right at their doorstep.
+                    We look forward to welcoming you to our hotel and providing you with a memorable and comfortable stay. Book your room today and experience the best of what our city has to offer!
                 </p>
             </div>
         </div>
