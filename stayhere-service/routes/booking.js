1be0dcf9b045f73710b94aae2998a71cb0b040b4
mira-dot <rakhimova.m@gmail.com>
2024-04-07 10:26:25 +0200

    BE - add validation for input and params

--- stayhere-service/routes/booking.js
+++ stayhere-service/routes/booking.js
@@ -14,6 +14,16 @@ router.get("/get", async (req, res) => {
 
 router.post("/create", async (req, res) => {
     try {
+        const isValid = req.body.FirstName?.length > 0 &&
+            req.body.LastName?.length > 0 &&
+            req.body.Email?.length > 0 &&
+            req.body.Address?.length > 0 &&
+            ["CASH", "CARD"].includes(req.body.PaymentType)
+        if (!isValid) {
+            res.status(500).send("All mandatory fields must be filled with valid data")
+            return
+        }
+
         const booking = await bookingsDAO.create(req.body)
         res.send(booking);
     } catch (error) {
@@ -23,6 +33,11 @@ router.post("/create", async (req, res) => {
 
 router.post("/status/:bookingId/:status", async (req, res) => {
     try {
+        if (!["ACCEPTED", "DECLINED", "CHECKEDIN", "CHECKEDOUT"].includes(req.params.status)) {
+            res.status(500).send("Invalid status")
+            return
+        }
+
         const booking = await bookingsDAO.changeStatus(req.params.bookingId, req.params.status);
         res.send(booking);
     } catch (error) {
