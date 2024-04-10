0353ce490c663e0f1f5bd4ff5f7847993719bfde
mira-dot <rakhimova.m@gmail.com>
2024-04-10 21:53:33 +0200

    change p html element usage

--- stayhere-web/src/pages/Home.js
+++ stayhere-web/src/pages/Home.js
@@ -9,12 +9,12 @@ const Home = () => {
             <div className="col-8">
                 <h2 className="text-center">Stay Here</h2>
                 <img className="float-start me-3" src="/images/main_image.jpg" width={600}/>
-                <p className="text-justify">
-                    <p className="text-center" style={{fontSize: "20px"}}><strong>Welcome to our cozy and charming hotel located near the city center!</strong></p>
+                <div className="text-justify">
+                    <h3 className="text-center" style={{fontSize: "20px", color: 'black'}}>Welcome to our cozy and charming hotel located near the city center!</h3>
                     With only 10 rooms, our hotel offers a personal touch and intimate atmosphere for our guests. Our garden and free parking provide a peaceful and convenient stay for those looking to explore the city without the hassle of parking.
                     Surrounded by entertaining and historical attractions, our hotel is the perfect base for those seeking to immerse themselves in the culture and history of the city. And with modern cafes and popular restaurants just a stone's throw away, guests can indulge in delicious dining options right at their doorstep.
                     We look forward to welcoming you to our hotel and providing you with a memorable and comfortable stay. Book your room today and experience the best of what our city has to offer!
-                </p>
+                </div>
             </div>
         </div>
     );
